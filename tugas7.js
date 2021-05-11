8a78c123664d46397a672735573bcc7b79d7e8ad
Faza <firdausauliafaza@gmail.com>
2021-05-11 09:01:22 +0700

    niomic.com

---
+++ tugas7.js
@@ -0,0 +1,14 @@
+var tinggibadan = [150, 160, 120, 170, 165, 177];
+
+for (i = 0; i < tinggibadan.length; i++) {
+  console.log(tinggibadan[i]);
+}
+
+for (let x of tinggibadan) {
+  console.log(x);
+}
+
+// Perbedaan antara for of dan perulangan biasa adalah
+// For of bisa menampung suatu variable didalam
+// variable baru misal contoh diatas let x of tinggi badan
+// maka x akan jadi variable baru untuk tinggi badan
